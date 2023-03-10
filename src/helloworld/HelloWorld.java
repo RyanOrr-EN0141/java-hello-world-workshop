1944ba34fa388c7be59de4e25030863cea50016c
RyanOrr-EN0141 <92317087+RyanOrr-EN0141@users.noreply.github.com>
2023-03-10 15:49:42 +0000

    Hello World

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,14 +4,22 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        //1. Print 'Hello World' to the console
-
-        //2. Assign the message ' and hello Aflac!' to a String
-
-        //3. Print the new message out to the console
-
-        //4. Change the code so that both messages are printed in one line
+        System.out.println("Hello World!");
 
     }
-
 }
+
+
+
+
+
+
+
+
+//1. Print 'Hello World' to the console
+
+//2. Assign the message ' and hello Aflac!' to a String
+
+//3. Print the new message out to the console
+
+//4. Change the code so that both messages are printed in one line
